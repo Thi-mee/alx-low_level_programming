729dc14f237540bc451d9c293640cd2ac3439f8a
Thimee <timswaggs1@outlook.com>
2022-06-03 09:12:10 -0700

    submission

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Entry point;
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+  char act;
+
+  for (act = 'a'; act <= 'z'; act++)
+  {
+    putchar(act);
+  }
+  putchar('\n');
+  return (0);
+
+}
