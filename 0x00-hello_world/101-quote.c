f8df17e4e7b659ef9adb25200146c2b652e3cc86
Thimee <timswaggs1@outlook.com>
2022-06-02 18:42:41 -0700

    submission

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of code
+ *
+ * Return: Always ends with 1 (success)
+ */
+int main(void)
+{
+  fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60, 1, stderr);
+}
