c9c24579c631684fd907d1dddf7e1a8e9b40eff4
Thimee <timswaggs1@outlook.com>
2022-06-03 09:46:28 -0700

    submission

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,10 +7,14 @@
 */
 int main(void)
 {
-	char num;
+	int num;
+	int converted_num;
 
-	for (num = '0'; num <= '9'; num++)
-	putchar(num);
+	for (num = 0; num <= 9; num++)
+	{
+		converted_num = num + '0';
+		putchar(num);
+	}
 	putchar('\n');
 	return (0);
 
