4f483d7459e59f06d50b09257ee00e66d673ddbb
Thimee <timswaggs1@outlook.com>
2022-06-03 09:14:41 -0700

    submission

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,13 +7,11 @@
 */
 int main(void)
 {
-  char act;
+	char act;
 
-  for (act = 'a'; act <= 'z'; act++)
-  {
-    putchar(act);
-  }
-  putchar('\n');
-  return (0);
+	for (act = 'a'; act <= 'z'; act++)
+	putchar(act);
+	putchar('\n');
+	return (0);
 
 }
