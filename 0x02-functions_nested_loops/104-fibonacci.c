857f1fc8859cdd4018011f61c9a0a7d2539331cb
Thimee <timswaggs1@outlook.com>
2022-06-07 16:52:51 -0700

    submission

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long fibonacci[98];
+
+	fibonacci[0] = 1;
+	fibonacci[1] = 2;
+	printf("%lu, %lu, ", fibonacci[0], fibonacci[1]);
+
+	for (i = 2; i < 98; i++)
+	{
+		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+		if (i == 97)
+		{
+			printf("%lu\n", fibonacci[i]);
+		}
+		printf("%lu, ", fibonacci[i]);
+	}
+	return (0);
+}
+
+
