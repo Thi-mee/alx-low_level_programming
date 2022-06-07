7cbb7bded465d75079a690ca7297671a0b746910
Thimee <timswaggs1@outlook.com>
2022-06-07 16:09:18 -0700

    submission

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,15 +9,15 @@
 int main(void)
 {
 	int i;
-	int fibonacci[50];
+	long int fibonacci[50];
 
 	fibonacci[0] = 1;
 	fibonacci[1] = 2;
-
+	printf("%ld, %ld, ", fibonacci[0], fibonacci[1])
 	for (i = 2; i < 50; i++)
 	{
 		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-		printf("%d, ", fibonacci[i]);
+		printf("%ld, ", fibonacci[i]);
 
 	}
 	
