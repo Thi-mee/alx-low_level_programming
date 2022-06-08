f26493a66c36cce76c8a9eece36cc9ca438d5507
Thimee <timswaggs1@outlook.com>
2022-06-07 18:09:48 -0700

    submission

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,23 +9,27 @@
 int main(void)
 {
 	int i;
-	unsigned long fibonacci[98], a_1, a_2, b_1, b_2, first, second, overflow;
+	unsigned long sum, a, b, a_1, a_2, b_1, b_2, first, second, overflow;
 
-	fibonacci[0] = 1;
-	fibonacci[1] = 2;
-	printf("%lu, %lu, ", fibonacci[0], fibonacci[1]);
+	a = 1;
+	b = 1;
+	printf("%lu", a);
 
 	for (i = 2; i < 92; i++)
 	{
-		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-		printf("%lu, ", fibonacci[i]);
+		sum = a + b;
+		a = b;
+		b = sum;
+		printf(", %lu", sum);
 	}
+
+	a_1 = a / 10000000000;
+	a_2 = a % 10000000000;
+	b_1 = b / 10000000000;
+	b_2 = b % 10000000000;
+
 	for (i = 92; i < 98; i++)
 	{
-		a_1 = fibonacci[i - 2] / 10000000000;
-		a_2 = fibonacci[i - 2] % 10000000000;
-		b_1 = fibonacci[i - 1] / 10000000000;
-		b_2 = fibonacci[i - 1] % 10000000000;
 
 		overflow = (a_2 + b_2) / 10000000000;
 		second = (a_2 + b_2) - (10000000000 * overflow);
@@ -37,6 +41,11 @@ int main(void)
 			return (0);
 		}
 		printf("%lu%lu, ", first, second);
+
+		a_1 = b_1;
+		a_2 = b_2;
+		b_1 = first;
+		b_2 = second;
 	}
 	return (0);
 }
