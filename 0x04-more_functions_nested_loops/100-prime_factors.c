fa03bf3c1032a2d87d8fd3c1bab9b8d5578fb31c
Thimee <timswaggs1@outlook.com>
2022-06-09 15:31:54 -0700

    submission

---
+++ 0x04-more_functions_nested_loops/100-prime_factors.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - finds the largest of
+ * prime factors
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	unsigned long num = 612852475143;
+	int divisor = 2;
+
+	while (divisor < num)
+	{
+		if (num % divisor == 0)
+		{
+			num \= divisor;
+			divisor = 2;
+		}
+		else
+			divisor++;
+	}
+	printf("%d\n", num);
+	return (0);
+}
