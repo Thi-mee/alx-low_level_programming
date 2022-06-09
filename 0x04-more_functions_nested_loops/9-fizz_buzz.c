e34096f736c740cdbec022847ca41417e35c67ef
Thimee <timswaggs1@outlook.com>
2022-06-09 14:34:47 -0700

    submission

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints fizzbuzz
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int count;
+
+	for (count = 1; count <= 100; count++)
+	{
+		if (count % 15 = 0)
+			printf("FizzBuzz ");
+		else if (count % 3 = 0)
+			printf("Fizz ");
+		else if (count % 5 = 0)
+			print("Buzz ");
+		else
+			printf("%d ", count);
+	}
+	return (0);
+}
