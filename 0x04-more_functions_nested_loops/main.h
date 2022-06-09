c274bcb6f0938fa1675617ed68222bfd7374bf52
Thimee <timswaggs1@outlook.com>
2022-06-09 13:16:05 -0700

    submission

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -8,7 +9,6 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
-int _putchar(char c);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
