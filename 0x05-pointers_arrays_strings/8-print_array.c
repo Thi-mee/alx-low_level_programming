470d5984f62c1423072aa0ca364d038573623c99
Thimee <timswaggs1@outlook.com>
2022-06-13 07:02:32 -0700

    submission

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_array - print's out an array
+ * @a: array
+ * @n: number to be printed
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf(a++);
+	}
+}
