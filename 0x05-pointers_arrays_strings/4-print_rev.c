9cd445a4d37f9d7b340d78ac32241f55a1c8a293
Thimee <timswaggs1@outlook.com>
2022-06-13 03:36:54 -0700

    submission

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ * @s: string
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	for (; len > 0; len--)
+	{
+		_putchar(s[len]);
+	}
+}
+		
