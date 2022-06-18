4e943b867df5347c0a26dcfa866837e660945050
Thimee <timswaggs1@outlook.com>
2022-06-17 19:28:42 -0700

    submission

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * print_number - print an integer, 
- * without using long, arrays, or pointers
+ * print_number - print an integer,
+ * without using long, arrays, 
+ * or pointers
  * @n: number to be printed
  * Return: void
  */
@@ -10,8 +11,10 @@
 void print_number(int n)
 {
 	unsigned int index, digit;
-	unsigned int number = (n < 0) ? n*-1 : n;
+	unsigned int number;
 	double divider = 1;
+
+	number = (n < 0)? n*-1 : n;
 
 	if (n == 0)
 		_putchar('0');
