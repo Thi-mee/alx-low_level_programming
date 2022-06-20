5526432056d92f4365813cfadbce033f3562ced4
Thimee <timswaggs1@outlook.com>
2022-06-20 15:11:27 -0700

    submission

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,17 +5,17 @@
  * @src: source to be copied from
  * @n: num bytes to copy from src
  *
- * Return: pointer to beginning of memory area dest
+ * Return: pointer to beginning of
+ * memory area dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
-	char *start = dest;
 
 	while (i < n)
 	{
 		*dest++ = *src++;
 		i++;
 	}
-	return (start);
+	return (dest);
 }
