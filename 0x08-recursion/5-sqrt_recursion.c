ccd819ecb14fa0aa4d49fececa9db6f35070addf
Thimee <timswaggs1@outlook.com>
2022-06-22 17:40:13 -0700

    submission

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * check_sqrt - checks for sqrt
+ * @n: number
+ * @i: times
+ * Return: integer
+ */
+
+int check_sqrt(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	return (check_sqrt(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the 
+ * natural square root of a number
+ * @n: integer
+ * Return: integer
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n <= 0)
+		retirn (-1);
+	return (check_sqrt(n, 0));
+}
