0a03b1e2d6f5ad5980a69bc6419ad09a7f119926
Thimee <timswaggs1@outlook.com>
2022-06-24 15:20:54 -0700

    submission

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints it's name
+ * @argc: number of arguments
+ * @argv: argument vector
+ * Return: integer
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%c\n", *argv);
+	return (0);
+}
