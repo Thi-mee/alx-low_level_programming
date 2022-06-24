70ad9b15c041213c7c7f310bcb8ac95ddb248749
Thimee <timswaggs1@outlook.com>
2022-06-24 14:55:11 -0700

    submission

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _islower - checks for lowercase character
+* @c: character to be checked
+* Return: 1 if lowercase and 0 otherwise
+*/
+int _islower(int c)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
