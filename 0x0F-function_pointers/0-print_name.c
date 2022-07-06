5301e5f8ef1de7008efe4e63424726fe3abe7b23
Thimee <timswaggs1@outlook.com>
2022-07-06 13:22:04 -0700

    submission

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,15 @@
 #include "function_pointers.h"
+
 /**
  * print_name - prints out name
  *
  * @name: name to be printed
  * @f: pointer to function
  */
+
 void print_name(char *name, void(*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
