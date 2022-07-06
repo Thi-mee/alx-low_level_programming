c868fa3c93b2bba85278f867d954c1a8484fc10d
Timi Adenuga <85906514+Thi-mee@users.noreply.github.com>
2022-07-06 22:08:26 +0100

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,6 +3,7 @@
 /**
  * int_index - finds char in an array
  * @array: array
+ * @size: size of the array
  * @cmp: pointer to function
  * Return: index of char
  */
@@ -11,7 +12,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0, j;
 
-	if (size <= 0) return (-1);
+	if (array == NULL || cmp == NULL)
+		return (-1);
+	if (size <= 0)
+		return (-1);
 	for (; i < size; i++)
 	{
 		j = cmp(*(array + i));
